d41ed43274a19afc7c6366fa89e4366d1705f8ad
vaibhav <vaibhavsharma38@gmail.com>
2016-04-05 18:04:54 +0530

    menu added in initiator

--- lib/Initiator.php
+++ lib/Initiator.php
@@ -13,6 +13,9 @@ class Initiator extends \Controller_Addon {
 		->setBaseURL('../vendor/xepan/projects/');
 
 		if($this->app->is_admin){
+			$m = $this->app->top_menu->addMenu('Projects');
+			$m->addItem('Dashboard','xepan_projects_dashboard');
+
 			$this->app->side_menu->addItem(['Dashboard','icon'=>'fa fa-dashboard',],'xepan_projects_dashboard');
 			$this->app->side_menu->addItem(['Add Project','icon'=>'fa fa-dashboard',],'xepan_projects_newproject');
 			$this->app->side_menu->addItem(['xEpan','icon'=>'fa fa-dashboard',],'xepan_projects_openproject');
