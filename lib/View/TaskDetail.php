84e4c519963b2880a6f6cb438905945c05375054
Gowrav Vishwakarma <gowravvishwakarma@gmail.com>
2016-04-09 11:30:19 +0530

    Some beautified code

---
+++ lib/View/TaskDetail.php
@@ -0,0 +1,191 @@
+<?php
+
+namespace xepan\projects;
+
+class View_TaskDetail extends \View{
+	
+	public $task_list_view=null;
+
+	function init(){
+		parent::init();
+		$self = $this;
+		$self_url=$this->app->url(null,['cut_object'=>$this->name]);
+
+
+		/***************************************************************************
+			Virtual page for assigning task.
+		***************************************************************************/	
+		$vp = $this->add('VirtualPage');
+		$vp->set(function($p)use($self,$self_url){
+						
+			$model_task = $p->add('xepan\projects\Model_Task')->load($_GET['task_id']);
+
+			$form = $p->add('Form');
+			$form->setModel($model_task,['employee_id']);
+			if($form->isSubmitted()){
+				throw new \Exception($model_task['employee'], 1);
+				
+				foreach($model_task->ref('SubTasks')->addCondition('employee_id',null) as $st){
+					$st['employee_id'] = $form['employee_id'];
+					$st->saveAndUnload();
+				}
+				$form->save();
+				$form->js('null',$self->js()->reload(null,null,$self_url))->univ()->closeDialog()->execute();
+			}
+
+		});
+
+		/***************************************************************************
+			Virtual page for assigning followers
+		***************************************************************************/
+		$vp2 = $this->add('VirtualPage');
+		$vp2->set(function($p){
+
+			$model_task = $p->add('xepan\projects\Model_Task');
+			$model_task->load($p->app->stickyGET('task_id'));
+
+			$model_employee = $p->add('xepan\hr\Model_Employee');
+			$model_follower_task_association = $p->add('xepan\projects\Model_Follower_Task_Association');
+			
+			$form = $p->add('Form');
+			$follower_field = $form->addField('line','name')->set(json_encode($model_task->getAssociatedFollowers()));
+
+			// Selectable for "task can have many followers" 
+
+			$follower_grid = $p->add('xepan\base\Grid');
+
+			$follower_grid->setModel($model_employee,['name']);
+			$follower_grid->addSelectable($follower_field);
+
+			if($form->isSubmitted()){
+
+				$model_task->removeAssociateFollowers();
+				
+				$selected_followers = array();
+			 	$selected_followers = json_decode($form['name'],true);
+
+				foreach ($selected_followers as $followers) {
+					$model_follower_task_association->addCondition('task_id',$_GET['task_id']);
+					$model_follower_task_association['employee_id'] = $followers;
+					$model_follower_task_association->saveAndUnload();
+				}
+
+				$form->js()->univ()->closeDialog()->execute(); 
+			}
+		});
+		
+
+		/***************************************************************************
+			js click function for assign task 
+		***************************************************************************/
+		$this->on('click','#assigntask',function($js,$data)use($vp){
+			return $js->univ()->dialogURL("ASSIGN TASK TO EMPLOYEE",$this->api->url($vp->getURL(),['task_id'=>$data['task_id']]));
+		});
+
+		/***************************************************************************
+			js click function for adding followers.
+		***************************************************************************/
+		$this->on('click','#addfollowers',function($js,$data)use($vp2){
+			return $js->univ()->dialogURL("ADD PEOPLE TO FOLLOW THIS TASK",$this->api->url($vp2->getURL(),['task_id'=>$data['task_id']]));
+		});
+	}
+
+	function setModel($model,$fields=null){		
+		$m = parent::setModel($model,$fields);
+		$this->add('xepan\base\Controller_Avatar',['options'=>['size'=>30],'name_field'=>'employee','default_value'=>'??']);
+
+		$task = $this->model;
+		$task_detail_view = $this;
+		$task_detail_view_url = $this->api->url(null,['cut_object'=>$task_detail_view->name]);
+		
+
+		$p_v = $task_detail_view->add('HtmlElement',null,'parent_name')
+				->setElement('a')
+				->setAttr('href','#')
+				->set($task['parent']);	
+		$p_v->js('click',$task_detail_view->js()->reload(['task_id'=>$task['parent_id']]));
+		
+
+		/***************************************************************************
+			Adding SubTasks.
+		***************************************************************************/
+		if($task['parent_id']==null){
+			
+			$subtask = $task_detail_view->add('Button',null,'subtask')->set('Add SubTasks');
+			$subtask->setAttr('data-id',$task->id);
+					
+			$subtask->on('click',null,function($js,$data)use($task_detail_view_url,$task_detail_view){
+				$js_new = [
+					$task_detail_view->js()->reload(['parent_id'=>$data['id']],null,$this->api->url($task_detail_view_url,['task_id'=>'']))
+				];
+				return $js_new;
+			});
+		}
+
+		/***************************************************************************
+			Showing SubTasks.
+		***************************************************************************/
+		if($task->loaded()){
+			$subtask = $task->ref('SubTasks');			
+			$task_detail_view->add('xepan\hr\Grid',null,'showsubtask',['view\subtasks'])->setModel($subtask);
+		}
+		
+		/***************************************************************************
+			Form to add tasks.
+		***************************************************************************/	
+		
+		$f = $task_detail_view->add('Form',null,'form');
+		$f->setModel($task,['task_name','description','starting_date','deadline']);
+		$f->addSubmit('Save');
+
+		if($f->isSubmitted()){
+			$parent_task = $this->add('xepan\projects\Model_Task')->tryLoad($parent_id?:0);
+			$f->model['employee_id'] = $parent_task['employee_id'];
+			$f->save();
+			$js=[$f->js()->univ()->successMessage('saved')];
+			$js[] = $task_detail_view->js()->reload(['task_id'=>$f->model->id,'parent_id'=>$task['parent_id']]);
+			$js[] = $task_list_view->js()->reload();
+			$this->js(null,$js)->execute();
+		}
+
+		/***************************************************************************
+			Form to add commnets on task.
+		***************************************************************************/
+		$comment_f = $task_detail_view->add('Form',null,'commentform');
+		$comment_f->addField('text','comment');	
+		$comment_f->addSubmit('ADD');
+
+		if($comment_f->isSubmitted()){
+			$model_comment = $this->add('xepan\projects\Model_Comment');
+
+			$model_comment->addCondition('task_id',$_GET['task_id']);
+			$model_comment->addCondition('name',$this->app->employee->id);
+
+			$model_comment['comment'] = $comment_f['comment'];
+			$model_comment->save();
+
+			$f->js()->univ()->successMessage('comment Saved')->execute();
+		}
+
+		/***************************************************************************
+			Grid to show comments
+		***************************************************************************/
+		$task_detail_view->add('xepan\hr\Grid',null,'commentgrid',['view\comment-grid'])->setModel('xepan\projects\Comment',['comment','name'])->addCondition('task_id',$task_id);
+
+		/***************************************************************************
+			Js to revert changes on cross icon click on task detail view
+		***************************************************************************/
+		$js_new = [
+			$this->owner->js()->_selector('#right_view')->hide(),
+			$this->owner->js()->_selector('#left_view')->removeClass('col-md-7'),
+			$this->owner->js()->_selector('#left_view')->addClass('col-md-12')
+		];
+		$task_detail_view->js('click',$js_new)->_selector('.glyphicon.glyphicon-remove');
+
+		return $m;
+	}
+
+	function defaultTemplate(){
+		return['view\taskdetail'];
+	}
+}
--- page/projectdetail.php
+++ page/projectdetail.php
@@ -25,9 +25,9 @@ class page_projectdetail extends \xepan\projects\page_sidemenu{
 		$task_list_view->add('xepan\base\Controller_Avatar',['options'=>['size'=>30],'name_field'=>'employee','default_value'=>'']);
 
 		// task detail view for showing/editing details of tasks.
-		$task_detail_view = $this->add('xepan\projects\View_Task',null,'rightview');
+		$task_detail_view = $this->add('xepan\projects\View_TaskDetail',['task_list_view'=>$task_list_view],'rightview');
 		$task_detail_view_url = $this->api->url(null,['cut_object'=>$task_detail_view->name]);
-		
+
 		$task = $this->add('xepan\projects\Model_Task');
 		$task->addCondition('project_id',$project_id);
 
@@ -43,83 +43,7 @@ class page_projectdetail extends \xepan\projects\page_sidemenu{
 
 		$task_detail_view->setModel($task);
 
-		/***************************************************************************
-			Adding SubTasks.
-		***************************************************************************/
-		
 
-		$p_v = $task_detail_view->add('HtmlElement',null,'parent_name')
-				->setElement('a')
-				->setAttr('href','#')
-				->set($task['parent']);	
-		$p_v->js('click',$task_detail_view->js()->reload(['task_id'=>$task['parent_id']]));
-
-		if($task['parent_id']==null){
-			
-			$subtask = $task_detail_view->add('Button',null,'subtask')->set('Add SubTasks');
-			$subtask->setAttr('data-id',$task_id);
-					
-			$subtask->on('click',null,function($js,$data)use($task_detail_view_url,$task_detail_view){
-				$js_new = [
-					$task_detail_view->js()->reload(['parent_id'=>$data['id']],null,$this->api->url($task_detail_view_url,['task_id'=>'']))
-				];
-				return $js_new;
-			});
-		}
-
-		/***************************************************************************
-			Showing SubTasks.
-		***************************************************************************/
-		if($task->loaded()){
-			$task->load($task_id);
-			$subtask = $task->ref('SubTasks');
-			
-			$task_detail_view->add('xepan\hr\Grid',null,'showsubtask',['view\subtasks'])->setModel($subtask);
-		}
-		
-		/***************************************************************************
-			Form to add tasks.
-		***************************************************************************/	
-		$f = $task_detail_view->add('Form',null,'form');
-		$f->setModel($task,['task_name','description','starting_date','deadline']);
-		$f->addSubmit('Save');
-
-		/***************************************************************************
-			Form to add commnets on task.
-		***************************************************************************/
-		$comment_f = $task_detail_view->add('Form',null,'commentform');
-		$comment_f->addField('text','comment');	
-		$comment_f->addSubmit('ADD');
-
-		/***************************************************************************
-			Handling Form Submission
-		***************************************************************************/
-		if($f->isSubmitted()){
-			$parent_task = $this->add('xepan\projects\Model_Task')->tryLoad($parent_id?:0);
-			$f->model['employee_id'] = $parent_task['employee_id'];
-			$f->save();
-			$js=[$f->js()->univ()->successMessage('saved')];
-			$js[] = $task_detail_view->js()->reload(['task_id'=>$f->model->id,'parent_id'=>$task['parent_id']]);
-			$js[] = $task_list_view->js()->reload();
-			$this->js(null,$js)->execute();
-		}
-
-		if($comment_f->isSubmitted()){
-			$model_comment = $this->add('xepan\projects\Model_Comment');
-
-			$model_comment->addCondition('task_id',$_GET['task_id']);
-			$model_comment->addCondition('name',$this->app->employee->id);
-
-			$model_comment['comment'] = $comment_f['comment'];
-			$model_comment->save();
-
-			$f->js()->univ()->successMessage('comment Saved')->execute();
-		}
-
-		/***************************************************************************
-			Grid to show comments
-		***************************************************************************/
-			$task_detail_view->add('xepan\hr\Grid',null,'commentgrid',['view\comment-grid'])->setModel('xepan\projects\Comment',['comment','name'])->addCondition('task_id',$task_id);
 		/***************************************************************************
 			Js to show task detail view
 		***************************************************************************/
@@ -134,15 +58,6 @@ class page_projectdetail extends \xepan\projects\page_sidemenu{
 			return $js_new;
 		});
 
-		/***************************************************************************
-			Js to revert changes on cross icon click on task detail view
-		***************************************************************************/
-		$js_new = [
-			$this->js()->_selector('#right_view')->hide(),
-			$this->js()->_selector('#left_view')->removeClass('col-md-7'),
-			$this->js()->_selector('#left_view')->addClass('col-md-12')
-		];
-		$task_detail_view->js('click',$js_new)->_selector('.glyphicon.glyphicon-remove');
 	}
 
 	function defaultTemplate(){
