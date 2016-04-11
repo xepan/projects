42cb260a2a84e7310c1b412c20cc7941d99888f9
vaibhav <vaibhavsharma38@gmail.com>
2016-04-11 15:00:13 +0530

    small changes made

--- lib/View/TopView.php
+++ lib/View/TopView.php
@@ -5,6 +5,11 @@ namespace xepan\projects;
 class View_TopView extends \View{
 	function init(){
 		parent::init();
+
+		$model_task = $this->add('xepan\projects\Model_Task')->addCondition('employee_id',$this->app->employee->id);
+		$model_task->addCondition('status','Pending');			
+		$pending_task = $model_task->count()->getOne();						
+		$this->add('View',null,'pending_task')->set("Pending Tasks: ".$pending_task);		
 	}
 
 	function defaultTemplate(){
