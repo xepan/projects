63b4b64cfb6f49d6bbe846afac837e146eb1436b
vaibhav <vaibhavsharma38@gmail.com>
2016-04-12 11:56:53 +0530

    progress bar added

--- lib/View/TopView.php
+++ lib/View/TopView.php
@@ -6,10 +6,55 @@ class View_TopView extends \View{
 	function init(){
 		parent::init();
 
-		$model_task = $this->add('xepan\projects\Model_Task')->addCondition('employee_id',$this->app->employee->id);
-		$model_task->addCondition('status','Pending');			
-		$pending_task = $model_task->count()->getOne();						
-		$this->add('View',null,'pending_task')->set("Pending Tasks: ".$pending_task);		
+	}
+
+	function setModel($model){
+		$model->addExpression('total_task')->set($model->refSQL('xepan\projects\Model_Task')->count());
+		$model->addExpression('pending_task_count')->set($model->refSQL('xepan\projects\Model_Task')->addCondition('status',['Pending'])->count());
+
+		$model->addExpression('completed_percentage')->set(function($m, $q){
+			return $m->dsql()->expr("ROUND(([1]-[0])/[1]*100,0)",[$m->getElement('pending_task_count'),$m->getElement('total_task')]);
+		});
+
+		$model->addExpression('self_task')->set($model->refSQL('xepan\projects\Model_Task')->addCondition('employee_id',$this->app->employee->id)->count());
+
+		$model->addExpression('self_pending_task')->set($model->refSQL('xepan\projects\Model_Task')->addCondition('employee_id',$this->app->employee->id)->addCondition('status',['Pending'])->count());
+
+		$model->addExpression('self_percentage')->set(function($m, $q){
+			return $m->dsql()->expr("ROUND(([1]-[0])/[1]*100,0)",[$m->getElement('self_pending_task'),$m->getElement('self_task')]);
+		});
+
+		$model->addExpression('self_color')->set(function($m){
+			return $m->dsql()->expr(
+					"IF([0]>75,'success',
+						if([0]>50,'warning',
+						if([0]>25,'info','danger'
+						)))",
+
+					  [
+						$m->getElement('self_percentage'),
+					  ]
+
+					);
+		});
+
+		$model->addExpression('color')->set(function($m){
+			return $m->dsql()->expr(
+					"IF([0]>75,'success',
+						if([0]>50,'warning',
+						if([0]>25,'info','danger'
+						)))",
+
+					  [
+						$m->getElement('completed_percentage'),
+					  ]
+
+					);
+		});
+
+		$m = parent::setModel($model);
+		
+		return $m;
 	}
 
 	function defaultTemplate(){
