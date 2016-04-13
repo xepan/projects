0dab4a74bbdd58712b72a97ef36256d95f5dae43
Gowrav Vishwakarma <gowravvishwakarma@gmail.com>
2016-04-13 09:29:28 +0530

    Employee related tasks done in project

--- lib/Model/Employee.php
+++ lib/Model/Employee.php
@@ -5,11 +5,52 @@ namespace xepan\projects;
 class Model_Employee extends \xepan\hr\Model_Employee{
 	function init(){
 		parent::init();
+		
+		$this->addExpression('running_task')->set(function($m,$q){
+			return $this->add('xepan\projects\Model_Timesheet',['table_alias'=>'running_task'])
+						->addCondition('endtime',null)
+						->addCondition('employee_id',$q->getField('id'))
+						->setOrder('starttime','desc')
+						->setLimit(1)
+						->fieldQuery('task');
+		});
 
-		$this->addExpression('team_members')->set("'Todo'");
-		$this->addExpression('project_name')->set("'Todo'");
-		$this->addExpression('total_task')->set("'Todo'");
-		$this->addExpression('completed_task')->set("'Todo'");
-		$this->addExpression('pending_task')->set("'Todo'");
+		$this->addExpression('running_task_id')->set(function($m,$q){
+			return $this->add('xepan\projects\Model_Timesheet',['table_alias'=>'running_task'])
+						->addCondition('endtime',null)
+						->addCondition('employee_id',$q->getField('id'))
+						->setOrder('starttime','desc')
+						->setLimit(1)
+						->fieldQuery('task_id');
+		});
+
+		$this->addExpression('running_task_since')->set(function($m,$q){
+			return $this->add('xepan\projects\Model_Timesheet',['table_alias'=>'running_task'])
+						->addCondition('endtime',null)
+						->addCondition('employee_id',$q->getField('id'))
+						->setOrder('starttime','desc')
+						->setLimit(1)
+						->fieldQuery('duration');
+		});
+
+		$this->addExpression('project')->set(function($m,$q){
+			$p=$this->add('xepan\projects\Model_Project');
+			$task_j = $p->join('task.project_id');
+			$task_j->addField('task_id','id');
+			$p->addCondition($q->expr('[0]=[1]',[$p->getElement('task_id'),$m->getField('running_task_id')]));
+			return $p->fieldQuery('name');
+		});
+
+		$this->hasMany('xepan\projects\Task','employee_id');
+
+		$this->addExpression('pending_tasks_count')->set(function ($m,$q){
+			return $m->refSQL('xepan\projects\Task')
+						->addCondition('status','Pending')
+						->count();
+		});
+
+
+
+		$this->addExpression('performance')->set("'Todo'");
 	}
 }
--- page/projectlive.php
+++ page/projectlive.php
@@ -9,13 +9,22 @@ class page_projectlive extends \xepan\projects\page_sidemenu{
 		$project_id = $this->app->stickyGET('project_id');
 		
 		$model_project = $this->add('xepan\projects\Model_Project')->load($project_id);
-		$model_employee = $this->add('xepan\projects\Model_Employee');
+
 
 		$top_view = $this->add('xepan\projects\View_TopView',null,'topview');
 		$top_view->setModel($model_project,['name']);
 
+		$model_employee = $this->add('xepan\projects\Model_Employee');
+		$model_employee->getElement('pending_tasks_count')->destroy();
+		$model_employee->addExpression('pending_tasks_count')->set(function ($m,$q)use($project_id){
+			return $m->refSQL('xepan\projects\Task')
+						->addCondition('status','Pending')
+						->addCondition('project_id',$project_id)
+						->count();
+		});
+		
 		$project_detail_grid = $this->add('xepan\hr\Grid',null,'grid');
-		$project_detail_grid->setModel($model_employee,['team_members','project_name','total_task','completed_task','pending_task']); 
+		$project_detail_grid->setModel($model_employee,['name','running_task','project','pending_tasks_count','running_task_since']); 
 	}
 
 	function defaultTemplate(){
