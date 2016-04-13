2c49ab4e30f0471af83a77013ace8b889a839ce6
anshusushil <milkysushil1920@gmail.com>
2016-04-13 13:31:11 +0530

    correction status and projectlive page

--- page/projectlive.php
+++ page/projectlive.php
@@ -25,7 +25,6 @@ class page_projectlive extends \xepan\projects\page_sidemenu{
 						->count();
 		});
 		
-		//$project_detail_grid = $this->add('xepan\hr\Grid',null,'grid');
 		$project_detail_grid=$this->add('xepan\hr\Grid',null,'grid',['view\status']);
 
 		$project_detail_grid->setModel($model_employee,['name','running_task','project','pending_tasks_count','running_task_since']); 
