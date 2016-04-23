16cd73b5cf04f9b1078e8eeca973944bef882004
vaibhav <vaibhavsharma38@gmail.com>
2016-04-23 13:33:58 +0530

    actual count of task shown in side menu

--- page/sidemenu.php
+++ page/sidemenu.php
@@ -9,18 +9,15 @@ class page_sidemenu extends \xepan\base\Page{
 
 		$this->app->side_menu->addItem(['Dashboard','icon'=>' fa fa-dashboard','badge'=>['10','swatch'=>' label label-primary label-circle pull-right']],'xepan_projects_projectdashboard');
 
-		$this->app->side_menu->addItem(['Projects','icon'=>' fa fa-edit','badge'=>[$project_count,'swatch'=>' label label-primary label-circle pull-right']],'xepan_projects_project');
-
-		// $projects_task = $this->add('xepan\projects\Model_Project');
-		// $projects_task->load($_GET['project_id']);
-
-		// $project_count = $projects_task->ref('xepan\projects\Task')->addCondition('status','Pending')->addCondition('employee_id',$this->app->employee->id)->count()->getOne();
+		$this->app->side_menu->addItem(['Projects','icon'=>' fa fa-sitemap','badge'=>[$project_count,'swatch'=>' label label-primary label-circle pull-right']],'xepan_projects_project');
 		
 		foreach ($projects as $project) {
 			$project_name = $project['name'];
 			$project_id = $project['id'];
 
-			$this->app->side_menu->addItem([$project_name],$this->app->url('xepan_projects_projectdetail',['project_id'=>$project_id]));
+			$task_count = $project->ref('xepan\projects\Task')->addCondition('employee_id',$this->app->employee_id)->addCondition('status','Pending')->count()->getOne();
+			
+			$this->app->side_menu->addItem([$project_name,'icon'=>' fa fa-tasks','badge'=>[$task_count,'swatch'=>' label label-primary label-circle pull-right']],$this->app->url('xepan_projects_projectdetail',['project_id'=>$project_id]));
 		}
 
 		
