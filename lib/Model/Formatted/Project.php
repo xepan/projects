72b420460764fb6ea233cf143a6e3c356e65a660
vaibhav <vaibhavsharma38@gmail.com>
2016-04-23 09:38:31 +0530

    missing "Project condition" added

--- lib/Model/Formatted/Project.php
+++ lib/Model/Formatted/Project.php
@@ -11,15 +11,15 @@ class Model_Formatted_Project extends \xepan\projects\Model_Project{
 			return  $m->add('xepan\projects\Model_Task')->addCondition('project_id',$_GET['project_id'])->count();
 		});
 		
-		$this->addExpression('pending_task_count')->set($this->refSQL('xepan\projects\Task')->addCondition('status',['Pending'])->count());
+		$this->addExpression('pending_task_count')->set($this->refSQL('xepan\projects\Task')->addCondition('status',['Pending'])->addCondition('project_id',$_GET['project_id'])->count());
 
 		$this->addExpression('completed_percentage')->set(function($m, $q){
 			return $m->dsql()->expr("ROUND(([1]-[0])/[1]*100,0)",[$m->getElement('pending_task_count'),$m->getElement('total_task')]);
 		});
 
-		$this->addExpression('self_task')->set($this->refSQL('xepan\projects\Task')->addCondition('employee_id',$this->app->employee->id)->count());
+		$this->addExpression('self_task')->set($this->refSQL('xepan\projects\Task')->addCondition('employee_id',$this->app->employee->id)->addCondition('project_id',$_GET['project_id'])->count());
 
-		$this->addExpression('self_pending_task')->set($this->refSQL('xepan\projects\Task')->addCondition('employee_id',$this->app->employee->id)->addCondition('status',['Pending'])->count());
+		$this->addExpression('self_pending_task')->set($this->refSQL('xepan\projects\Task')->addCondition('employee_id',$this->app->employee->id)->addCondition('project_id',$_GET['project_id'])->addCondition('status',['Pending'])->count());
 
 		$this->addExpression('self_percentage')->set(function($m, $q){
 			return $m->dsql()->expr("ROUND(([1]-[0])/[1]*100,0)",[$m->getElement('self_pending_task'),$m->getElement('self_task')]);
@@ -59,7 +59,7 @@ class Model_Formatted_Project extends \xepan\projects\Model_Project{
 		});
 		
 		
-		$this->addExpression('critical_pending_task_count')->set($this->refSQL('xepan\projects\Task')->addCondition('status',['Pending'])->addCondition('priority','90')->count());
+		$this->addExpression('critical_pending_task_count')->set($this->refSQL('xepan\projects\Task')->addCondition('project_id',$_GET['project_id'])->addCondition('status',['Pending'])->addCondition('priority','90')->count());
 
 		$this->addExpression('critical_completed_percentage')->set(function($m, $q){
 			return $m->dsql()->expr("ROUND(([1]-[0])/[1]*100,0)",[$m->getElement('critical_pending_task_count'),$m->getElement('total_critical_task')]);
@@ -71,7 +71,7 @@ class Model_Formatted_Project extends \xepan\projects\Model_Project{
 		});
 		
 		
-		$this->addExpression('high_pending_task_count')->set($this->refSQL('xepan\projects\Task')->addCondition('status',['Pending'])->addCondition('priority','75')->count());
+		$this->addExpression('high_pending_task_count')->set($this->refSQL('xepan\projects\Task')->addCondition('status',['Pending'])->addCondition('project_id',$_GET['project_id'])->addCondition('priority','75')->count());
 
 		$this->addExpression('high_completed_percentage')->set(function($m, $q){
 			return $m->dsql()->expr("ROUND(([1]-[0])/[1]*100,0)",[$m->getElement('high_pending_task_count'),$m->getElement('total_high_task')]);
@@ -84,7 +84,7 @@ class Model_Formatted_Project extends \xepan\projects\Model_Project{
 		});
 		
 		
-		$this->addExpression('medium_pending_task_count')->set($this->refSQL('xepan\projects\Task')->addCondition('status',['Pending'])->addCondition('priority','50')->count());
+		$this->addExpression('medium_pending_task_count')->set($this->refSQL('xepan\projects\Task')->addCondition('status',['Pending'])->addCondition('project_id',$_GET['project_id'])->addCondition('priority','50')->count());
 
 		$this->addExpression('medium_completed_percentage')->set(function($m, $q){
 			return $m->dsql()->expr("ROUND(([1]-[0])/[1]*100,0)",[$m->getElement('medium_pending_task_count'),$m->getElement('total_medium_task')]);
@@ -96,41 +96,41 @@ class Model_Formatted_Project extends \xepan\projects\Model_Project{
 		});
 		
 		
-		$this->addExpression('low_pending_task_count')->set($this->refSQL('xepan\projects\Task')->addCondition('status',['Pending'])->addCondition('priority','25')->count());
+		$this->addExpression('low_pending_task_count')->set($this->refSQL('xepan\projects\Task')->addCondition('status',['Pending'])->addCondition('project_id',$_GET['project_id'])->addCondition('priority','25')->count());
 
 		$this->addExpression('low_completed_percentage')->set(function($m, $q){
 			return $m->dsql()->expr("ROUND(([1]-[0])/[1]*100,0)",[$m->getElement('low_pending_task_count'),$m->getElement('total_low_task')]);
 		});
 
 
-		$this->addExpression('self_critical_task')->set($this->refSQL('xepan\projects\Task')->addCondition('employee_id',$this->app->employee->id)->addCondition('priority','90')->count());
+		$this->addExpression('self_critical_task')->set($this->refSQL('xepan\projects\Task')->addCondition('employee_id',$this->app->employee->id)->addCondition('project_id',$_GET['project_id'])->addCondition('priority','90')->count());
 
-		$this->addExpression('critical_self_pending_task')->set($this->refSQL('xepan\projects\Task')->addCondition('employee_id',$this->app->employee->id)->addCondition('status',['Pending'])->addCondition('priority','90')->count());
+		$this->addExpression('critical_self_pending_task')->set($this->refSQL('xepan\projects\Task')->addCondition('employee_id',$this->app->employee->id)->addCondition('status',['Pending'])->addCondition('project_id',$_GET['project_id'])->addCondition('priority','90')->count());
 
 		$this->addExpression('critical_self_percentage')->set(function($m, $q){
 			return $m->dsql()->expr("ROUND(([1]-[0])/[1]*100,0)",[$m->getElement('critical_self_pending_task'),$m->getElement('self_critical_task')]);
 		});
 
 
-		$this->addExpression('self_high_task')->set($this->refSQL('xepan\projects\Task')->addCondition('employee_id',$this->app->employee->id)->addCondition('priority','75')->count());
+		$this->addExpression('self_high_task')->set($this->refSQL('xepan\projects\Task')->addCondition('employee_id',$this->app->employee->id)->addCondition('project_id',$_GET['project_id'])->addCondition('priority','75')->count());
 
-		$this->addExpression('high_self_pending_task')->set($this->refSQL('xepan\projects\Task')->addCondition('employee_id',$this->app->employee->id)->addCondition('status',['Pending'])->addCondition('priority','75')->count());
+		$this->addExpression('high_self_pending_task')->set($this->refSQL('xepan\projects\Task')->addCondition('employee_id',$this->app->employee->id)->addCondition('status',['Pending'])->addCondition('project_id',$_GET['project_id'])->addCondition('priority','75')->count());
 
 		$this->addExpression('high_self_percentage')->set(function($m, $q){
 			return $m->dsql()->expr("ROUND(([1]-[0])/[1]*100,0)",[$m->getElement('high_self_pending_task'),$m->getElement('self_high_task')]);
 		});
 
-		$this->addExpression('self_medium_task')->set($this->refSQL('xepan\projects\Task')->addCondition('employee_id',$this->app->employee->id)->addCondition('priority','50')->count());
+		$this->addExpression('self_medium_task')->set($this->refSQL('xepan\projects\Task')->addCondition('employee_id',$this->app->employee->id)->addCondition('project_id',$_GET['project_id'])->addCondition('priority','50')->count());
 
-		$this->addExpression('medium_self_pending_task')->set($this->refSQL('xepan\projects\Task')->addCondition('employee_id',$this->app->employee->id)->addCondition('status',['Pending'])->addCondition('priority','50')->count());
+		$this->addExpression('medium_self_pending_task')->set($this->refSQL('xepan\projects\Task')->addCondition('employee_id',$this->app->employee->id)->addCondition('status',['Pending'])->addCondition('project_id',$_GET['project_id'])->addCondition('priority','50')->count());
 
 		$this->addExpression('medium_self_percentage')->set(function($m, $q){
 			return $m->dsql()->expr("ROUND(([1]-[0])/[1]*100,0)",[$m->getElement('medium_self_pending_task'),$m->getElement('self_medium_task')]);
 		});
 
-		$this->addExpression('self_low_task')->set($this->refSQL('xepan\projects\Task')->addCondition('employee_id',$this->app->employee->id)->addCondition('priority','25')->count());
+		$this->addExpression('self_low_task')->set($this->refSQL('xepan\projects\Task')->addCondition('employee_id',$this->app->employee->id)->addCondition('project_id',$_GET['project_id'])->addCondition('priority','25')->count());
 
-		$this->addExpression('low_self_pending_task')->set($this->refSQL('xepan\projects\Task')->addCondition('employee_id',$this->app->employee->id)->addCondition('status',['Pending'])->addCondition('priority','25')->count());
+		$this->addExpression('low_self_pending_task')->set($this->refSQL('xepan\projects\Task')->addCondition('employee_id',$this->app->employee->id)->addCondition('project_id',$_GET['project_id'])->addCondition('status',['Pending'])->addCondition('priority','25')->count());
 
 		$this->addExpression('low_self_percentage')->set(function($m, $q){
 			return $m->dsql()->expr("ROUND(([1]-[0])/[1]*100,0)",[$m->getElement('low_self_pending_task'),$m->getElement('self_low_task')]);
