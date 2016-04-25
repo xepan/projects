fdd2aa85ac90ce9a11ce4217eedd101b7207b3a6
vaibhav <vaibhavsharma38@gmail.com>
2016-04-25 11:00:00 +0530

    bud solved

--- page/sidemenu.php
+++ page/sidemenu.php
@@ -14,8 +14,8 @@ class page_sidemenu extends \xepan\base\Page{
 		foreach ($projects as $project) {
 			$project_name = $project['name'];
 			$project_id = $project['id'];
-
-			$task_count = $project->ref('xepan\projects\Task')->addCondition('employee_id',$this->app->employee_id)->addCondition('status','Pending')->count()->getOne();
+			
+			$task_count = $project->ref('xepan\projects\Task')->addCondition('employee_id',$this->app->employee->id)->addCondition('status','Pending')->count()->getOne();
 			
 			$this->app->side_menu->addItem([$project_name,'icon'=>' fa fa-tasks','badge'=>[$task_count,'swatch'=>' label label-primary label-circle pull-right']],$this->app->url('xepan_projects_projectdetail',['project_id'=>$project_id]));
 		}
