dee7e6b4a362f69b426eb8edd026e6701ce61362
vaibhav <vaibhavsharma38@gmail.com>
2016-04-30 19:11:18 +0530

    gantt tool

--- page/files.php
+++ page/files.php
@@ -12,10 +12,23 @@ class page_files extends \xepan\projects\page_sidemenu{
 		
 		$top_view = $this->add('xepan\projects\View_TopView',null,'topview');
 		$top_view->setModel($model_project)->load($project_id);
-		$this->add('xepan\projects\View_File',null,'leftview');
+		$view = $this->add('xepan\projects\View_File',null,'leftview');
+
 	}
 
 	function defaultTemplate(){
 		return['page\projectdetail'];
+	}
+
+	function render(){
+		$this->js(true)->_load('gantt\ganttUtilities')
+					   ->_load('gantt\ganttTask')
+					   ->_load('gantt\ganttDrawerSVG')
+					   ->_load('gantt\ganttGridEditor')
+					   ->_load('gantt\ganttMaster')
+					   ->_load('gantt\ganttTask');
+
+	    $this->js(true)->_selector('#calendar')->GanttMaster();
+		parent::render();
 	}	
 }
