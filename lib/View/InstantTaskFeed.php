03ff17785a95faeb31c72df718045403ca76ab9d
vaibhav <vaibhavsharma38@gmail.com>
2016-05-28 18:39:59 +0530

    bug solved in instant task feed

--- lib/View/InstantTaskFeed.php
+++ lib/View/InstantTaskFeed.php
@@ -54,6 +54,19 @@ class View_InstantTaskFeed extends \View{
 			];
 
 		if($form->isSubmitted()){
+			$model_close_timesheet = $this->add('xepan\projects\Model_Timesheet');
+
+			$model_close_timesheet->addCondition('employee_id',$this->app->employee->id);
+			$model_close_timesheet->setOrder('starttime','desc');
+			$model_close_timesheet->tryLoadAny();
+
+			if($model_close_timesheet->loaded()){
+				if(!$model_close_timesheet['endtime']){
+					$model_close_timesheet['endtime'] = $this->app->now;
+					$model_close_timesheet->save();
+				}
+			}
+			
 			if(!is_numeric($form['task'])){
 				$model_task->addCondition('employee_id',$this->app->employee->id);
 				$model_task['task_name'] = $form['task'];
