c0694ad7515fb009f0f29319ad37f5f09645289c
vaibhav <vaibhavsharma38@gmail.com>
2016-05-28 17:27:50 +0530

    mini task done

--- lib/View/InstantTaskFeed.php
+++ lib/View/InstantTaskFeed.php
@@ -63,7 +63,7 @@ class View_InstantTaskFeed extends \View{
 
 				$model_timesheet->addCondition('employee_id',$this->app->employee->id);
 				$model_timesheet->addCondition('task_id',$model_task->id);
-				$model_timesheet['starttime'] = $time;
+				$model_timesheet['starttime'] = $this->app->now;
 				$model_timesheet->save();
 				return;
 			}
@@ -71,7 +71,7 @@ class View_InstantTaskFeed extends \View{
 			$model_timesheet->addCondition('employee_id',$this->app->employee->id);
 			$model_timesheet->addCondition('task_id',$form['task']);
 			$model_timesheet['remark'] = $form['remark'];
-			$model_timesheet['starttime'] = $time;
+			$model_timesheet['starttime'] = $this->app->now;
 			$model_timesheet->save();
 			return;
 		}
