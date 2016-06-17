2a8d8c642056b2f06291ccb3b05bc69d13ef1ff1
vaibhav <vaibhavsharma38@gmail.com>
2016-06-17 14:21:25 +0530

    changes made in quick search grid

--- lib/Model/Project.php
+++ lib/Model/Project.php
@@ -75,30 +75,36 @@ class Model_Project extends \xepan\base\Model_Table
 		}
 	}
 
-	function quickSearch($app,$search_string,$view){
+	function quickSearch($app,$search_string,&$result_array){		
 		$this->addExpression('Relevance')->set('MATCH(name, description, type) AGAINST ("'.$search_string.'" IN NATURAL LANGUAGE MODE)');
 		$this->addCondition('Relevance','>',0);
  		$this->setOrder('Relevance','Desc');
+ 		
  		if($this->count()->getOne()){
- 			$pc = $view->add('Completelister',null,null,['view/quicksearch-project-grid']);
- 			$pc->setModel($this);
-    		$pc->addHook('formatRow',function($g){
-    			$g->current_row_html['url'] = $this->app->url('xepan_projects_projectdetail',['project_id'=> $g->model->id]);	
-     		});	
+ 			foreach ($this->getRows() as $data) { 				
+ 				$result_array[] = [
+ 					'image'=>null,
+ 					'title'=>$data['name'],
+ 					'relevency'=>$data['Relevance'],
+ 					'url'=>$this->app->url('xepan_projects_projectdetail',['status'=>$data['status'],'project_id'=>$data['id']])->getURL(),
+ 				];
+ 			}
 		}
 
 		$task = $this->add('xepan\projects\Model_Task');
 		$task->addExpression('Relevance')->set('MATCH(task_name, description, status, type) AGAINST ("'.$search_string.'" IN NATURAL LANGUAGE MODE)');
 		$task->addCondition('Relevance','>',0);
  		$task->setOrder('Relevance','Desc');
+ 		
  		if($task->count()->getOne()){
- 			$tc = $view->add('Completelister',null,null,['view/quicksearch-project-grid']);
- 			$tc->setModel($task);
-    		$tc->addHook('formatRow',function($g){
-    			$g->current_row_html['url'] = $this->app->url('xepan_projects_projectdetail',['project_id'=> $g->model['project_id']]);	
-     		});	
+ 			foreach ($task->getRows() as $data) { 
+ 				$result_array[] = [
+ 					'image'=>null,
+ 					'title'=>$data['task_name'],
+ 					'relevency'=>$data['Relevance'],
+ 					'url'=>$this->app->url('xepan_projects_projectdetail',['status'=>$data['status'],'project_id'=>$data['id']])->getURL(),
+ 				];
+ 			}
 		}
-
-
 	}	
 }
