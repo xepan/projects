bb1ea963a18272a3714f6d72a480fbd01d50bced
vaibhav <vaibhavsharma38@gmail.com>
2016-07-19 10:29:20 +0530

    testing for field

--- page/test.php
+++ page/test.php
@@ -5,6 +5,12 @@ class page_test extends \Page{
 	function init(){
 		parent::init();
 
-		$this->add('xepan\projects\View_InstantTaskFeed');
+		$form = $this->add('Form');
+		$fld = $form->addField('DateTimePicker','time');
+		$form->addSubmit('Submit');
+
+		if($form->isSubmitted()){
+			throw new \Exception($form['time']);
+		}
 	}
 }
