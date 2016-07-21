6e27eed3ee205d0a677630c8f33ae0d51ba85878
vaibhav <vaibhavsharma38@gmail.com>
2016-07-21 19:20:36 +0530

    saved values displayed in multiselect

--- lib/View/TaskReminder.php
+++ lib/View/TaskReminder.php
@@ -19,7 +19,17 @@ class View_TaskReminder extends \View{
 		$reminder_crud->setModel($task,['task_name','notify_to','employee_id','starting_date','remind_via','remind_value','remind_unit']);
 
 		if($reminder_crud->isEditing()){
-			// $crud->form->getElement('employee_id')->
+			$task = $this->add('xepan\projects\Model_Task')->load($reminder_crud->model->id);
+
+			$temp = [];
+			$temp = explode(',', $task['notify_to']);
+
+			$temp1 = [];
+			$temp1 = explode(',', $task['remind_via']);
+
+			$reminder_crud->form->getElement('notify_to')->set($temp)->js(true)->trigger('changed');
+			$reminder_crud->form->getElement('remind_via')->set($temp1)->js(true)->trigger('changed');
+
 			$reminder_crud->form->getElement('notify_to')
 							->setAttr(['multiple'=>'multiple']);
 
@@ -27,12 +37,15 @@ class View_TaskReminder extends \View{
 							->setAttr(['multiple'=>'multiple']);
 
 			$reminder_crud->form->getElement('employee_id')->getModel()->addCondition('status',"Active");
+		
+			if($reminder_crud->form->isSubmitted()){
+				if($task['notify_to']) $task['notify_to'] = '';
+				if($task['remind_via']) $task['remind_via'] = '';
+			}
 		}
 
 		$reminder_crud->grid->addHook('formatRow',function($g){						
 			$g->current_row['reminder_time'] = date("Y-m-d H:i:s", strtotime('-'.$g->model['remind_value'].' '.$g->model['remind_unit'], strtotime($g->model['starting_date'])));		
-		});
-		
-		// $task->reminder();
+		});		
 	}
 }
