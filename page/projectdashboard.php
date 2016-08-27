0da43b8d5692c004e384f0a3be1aade8d48b4cef
vaibhav <vaibhavsharma38@gmail.com>
2016-08-27 19:49:16 +0530

    dashboard added

--- page/projectdashboard.php
+++ page/projectdashboard.php
@@ -7,6 +7,27 @@ class page_projectdashboard extends \xepan\projects\page_sidemenu{
 	function init(){
 		parent::init();
 
-		$this->add('View_Info')->set('Graphical information about projects');
+		$project = $this->add('xepan\projects\Model_project');
+		$tasks = $this->add('xepan\projects\Model_task');
+		
+		$this->template->trySet('projects',$project->count());
+		$this->template->trySet('tasks',$tasks->count());
+		
+		$tasks->addCondition('status','Pending');
+		$this->template->trySet('pending_tasks',$tasks->count());
+
+		$completed_tasks = $this->add('xepan\projects\Model_task');
+		$completed_tasks->addCondition('status','Completed');
+		$this->template->trySet('completed_tasks',$tasks->count());
+
+		$model_formatted_projects = $this->add('xepan\projects\Model_Formatted_Project');
+		$model_formatted_projects->addCondition('status','Active');
+
+		$project_overview = $this->add('xepan\base\Grid',null,'project_overview',['view\dashboard\projectoverview']);
+		$project_overview->setModel($model_formatted_projects);		
+	}
+
+	function defaultTemplate(){
+		return ['page\dashboard'];
 	}
 }
