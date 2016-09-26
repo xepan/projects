314bdc3d7a978f5aae6ce9329b516a73113c21bf
vaibhav <vaibhavsharma38@gmail.com>
2016-09-26 19:14:54 +0530

    report page adde

--- lib/Initiator.php
+++ lib/Initiator.php
@@ -34,6 +34,7 @@ class Initiator extends \Controller_Addon {
 				$m->addItem([$project_name,'icon'=>' fa fa-tasks'],$this->app->url('xepan_projects_projectdetail',['project_id'=>$project_id]),['project_id']);
 			}
 			$m->addItem(['Configuration','icon'=>' fa fa-cog'],'xepan_projects_layout');
+			$m->addItem(['Reports','icon'=>' fa fa-cog'],'xepan_projects_report');
 		}
 
 		$search_project = $this->add('xepan\projects\Model_Project');
---
+++ page/report.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace xepan\projects;
+
+class page_report extends \xepan\base\Page{
+	public $title = "Project Report";
+
+	function init(){
+		parent::init();
+		
+	}
+}
