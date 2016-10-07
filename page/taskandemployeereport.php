b65ff82d04673cfcd33747e8f087155846108a84
vaibhav <vaibhavsharma38@gmail.com>
2016-10-07 11:47:07 +0530

    task and employee report completed

--- page/taskandemployeereport.php
+++ page/taskandemployeereport.php
@@ -13,70 +13,87 @@ class page_taskandemployeereport extends page_reportsidebar{
 		********************************************************************/
 		$from_date = $this->app->stickyGET('from_date')?:$this->app->today;
 		$to_date = $this->app->stickyGET('to_date')?:$this->app->nextDate($this->app->today);
-
+		$employee_id = $this->app->stickyGET('employee_id'); 
+		$status = $this->app->stickyGET('status'); 
 		/*******************************************************************
 		 EMPLOYEE MODEL AND EXPRESSIONS
 		********************************************************************/
-		$employee = $this->add('xepan\hr\Model_Employee');
+		$task = $this->add('xepan\projects\Model_Task');
+		if($employee_id)								
+			$task->addCondition('employee_id',$employee_id);
+		if($status)
+			$task->addCondition('status',$status);
+		if($from_date)
+			$task->addCondition('created_at','>=',$from_date);
+		if($from_date)
+			$task->addCondition('created_at','<=',$to_date);
 
-		// total number of tasks alloted to employee
-		$employee->addExpression('total_tasks')->set(function($m,$q){
-			return $this->add('xepan\projects\Model_Task')
-				        ->addCondition('employee_id',$m->getElement('id'))
-				        ->count();
+		$task->addExpression('time_consumed')->set(function($m,$q){
+			$time_sheet = $this->add('xepan\projects\Model_Timesheet',['table_alias'=>'total_duration']);
+			$time_sheet->addCondition('task_id',$q->getField('id'));
+			return $time_sheet->dsql()->del('fields')->field($q->expr('sec_to_time(SUM([0]))',[$time_sheet->getElement('duration')]));
 		});
 
-		$employee->addExpression('total_pending_tasks')->set(function($m,$q){
-			return $this->add('xepan\projects\Model_Task')
-				        ->addCondition('employee_id',$m->getElement('id'))
-				        ->addCondition('status','Pending')
-				        ->count();
-		});		
+		// DONT REMOVE THE COMENTED EXPRESSIONS, IT HELPS ME FREQUENTLY
 
-		// total number of tasks employee worked on 
-		// $employee->addExpression('tasks_worked_on')->set(function($m,$q){
-		// 	return $this->add('xepan\projects\Model_Timesheet')
+		// // total number of tasks alloted to employee
+		// $employee->addExpression('total_tasks')->set(function($m,$q){
+		// 	return $this->add('xepan\projects\Model_Task')
 		// 		        ->addCondition('employee_id',$m->getElement('id'))
-		// 		        ->_dsql()->group('task_id')
 		// 		        ->count();
 		// });
 
-		// total hours alloted 
-		$employee->addExpression('total_hours_alloted')->set(function($m,$q){
-			$task_m = $this->add('xepan\projects\Model_Task',['table_alias'=>'emptsk'])
-				           ->addCondition('employee_id',$q->getField('id'));
-			$task_m->addExpression('diff_time')->set(function($m,$q){
-				return $q->expr('TIMESTAMPDIFF([0],[1],[2])',
-					['HOUR',$q->getField('starting_date'),$q->getField('deadline')]);
-			});
+		// $employee->addExpression('total_pending_tasks')->set(function($m,$q){
+		// 	return $this->add('xepan\projects\Model_Task')
+		// 		        ->addCondition('employee_id',$m->getElement('id'))
+		// 		        ->addCondition('status','Pending')
+		// 		        ->count();
+		// });		
 
-			return $task_m->_dsql()->del('fields')->field($q->expr('sum([0])',[$task_m->getElement('diff_time')]));
-		});
+		// // total number of tasks employee worked on 
+		// // $employee->addExpression('tasks_worked_on')->set(function($m,$q){
+		// // 	return $this->add('xepan\projects\Model_Timesheet')
+		// // 		        ->addCondition('employee_id',$m->getElement('id'))
+		// // 		        ->_dsql()->group('task_id')
+		// // 		        ->count();
+		// // });
 
-		// total amount of estimate hours
-		$employee->addExpression('total_estimated_hours')->set(function($m,$q){
-			$task_m = $this->add('xepan\projects\Model_Task')
-				           ->addCondition('employee_id',$m->getElement('id'));
+		// // total hours alloted 
+		// $employee->addExpression('total_hours_alloted')->set(function($m,$q){
+		// 	$task_m = $this->add('xepan\projects\Model_Task',['table_alias'=>'emptsk'])
+		// 		           ->addCondition('employee_id',$q->getField('id'));
+		// 	$task_m->addExpression('diff_time')->set(function($m,$q){
+		// 		return $q->expr('TIMESTAMPDIFF([0],[1],[2])',
+		// 			['HOUR',$q->getField('starting_date'),$q->getField('deadline')]);
+		// 	});
+
+		// 	return $task_m->_dsql()->del('fields')->field($q->expr('sum([0])',[$task_m->getElement('diff_time')]));
+		// });
+
+		// // total amount of estimate hours
+		// $employee->addExpression('total_estimated_hours')->set(function($m,$q){
+		// 	$task_m = $this->add('xepan\projects\Model_Task')
+		// 		           ->addCondition('employee_id',$m->getElement('id'));
 			
-			return $task_m->_dsql()->del('fields')->field($q->expr('sum([0])',[$task_m->getElement('estimate_time')]));
-		});
+		// 	return $task_m->_dsql()->del('fields')->field($q->expr('sum([0])',[$task_m->getElement('estimate_time')]));
+		// });
 
-		// total amount of time employee worked 
-		$employee->addExpression('total_minutes_taken')->set(function($m,$q){
-			$timesheet_m = $this->add('xepan\projects\Model_Timesheet')
-				                ->addCondition('employee_id',$m->getElement('id'));
-			$timesheet_m->addExpression('diff_time')->set(function($m,$q){
-				return $q->expr('TIMESTAMPDIFF([0],[1],[2])',
-					['MINUTE',$q->getField('starttime'),$q->getField('endtime')]);
-			});
+		// // total amount of time employee worked 
+		// $employee->addExpression('total_minutes_taken')->set(function($m,$q){
+		// 	$timesheet_m = $this->add('xepan\projects\Model_Timesheet')
+		// 		                ->addCondition('employee_id',$m->getElement('id'));
+		// 	$timesheet_m->addExpression('diff_time')->set(function($m,$q){
+		// 		return $q->expr('TIMESTAMPDIFF([0],[1],[2])',
+		// 			['MINUTE',$q->getField('starttime'),$q->getField('endtime')]);
+		// 	});
 			
-			return $timesheet_m->_dsql()->del('fields')->field($q->expr('sum([0])',[$timesheet_m->getElement('diff_time')]));
-		});
+		// 	return $timesheet_m->_dsql()->del('fields')->field($q->expr('sum([0])',[$timesheet_m->getElement('diff_time')]));
+		// });
 
-		// total_minutes_taken converted in hours
-		$employee->addExpression('total_hours_taken')->set(function($m,$q){
-			return $q->expr('([0])/60',[$m->getElement('total_minutes_taken')]);
-		});
+		// // total_minutes_taken converted in hours
+		// $employee->addExpression('total_hours_taken')->set(function($m,$q){
+		// 	return $q->expr('([0])/60',[$m->getElement('total_minutes_taken')]);
+		// });
 
 		/*******************************************************************
 	 	 FORM TO ENTER INFORMATION
@@ -84,6 +101,10 @@ class page_taskandemployeereport extends page_reportsidebar{
 		$form = $this->add('Form');
 		$form->addField('DatePicker','from_date')->set($this->app->today);
 		$form->addField('DatePicker','to_date')->set($this->app->today);
+		$emp_field = $form->addField('Dropdown','status')->setValueList(['Pending'=>'Pending','Completed'=>'Completed','Submitted'=>'Submitted']);
+		$emp_field = $form->addField('Dropdown','employee');
+		$emp_field->setEmptyText('Please select a employee');
+		$emp_field->setModel('xepan\hr\Model_Employee');
 		$form->addSubmit('Get Report')->addclass('btn btn-primary btn-sm btn-block');	
 	
 		// GRID WILL BE ADDED ON THIS VIEW
@@ -92,8 +113,10 @@ class page_taskandemployeereport extends page_reportsidebar{
 		/*******************************************************************
 		 ADDING GRID ON VIEW AND SETTING MODEL
 		********************************************************************/
-		$grid = $view->add('Grid');			
-		$grid->setModel($employee,['name','total_tasks','total_pending_tasks','total_hours_alloted','total_estimated_hours','total_hours_taken']);
+		if($employee_id){		
+			$grid = $view->add('Grid');			
+			$grid->setModel($task,['task_name','starting_date','deadline','estimate_time','time_consumed']);
+		}
 		/*******************************************************************
 		 HANDLING FORM SUBMISSION
 		********************************************************************/
@@ -101,6 +124,8 @@ class page_taskandemployeereport extends page_reportsidebar{
 			$array = [
 						'from_date'=>$form['from_date'],
 						'to_date'=>$form['to_date'],
+						'employee_id'=>$form['employee'],
+						'status'=>$form['status']
 					 ];
 			$view->js()->reload($array)->execute();
 		}
