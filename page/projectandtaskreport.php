291e432750e390e8da74213cd2ddf7b888b568ef
vaibhav <vaibhavsharma38@gmail.com>
2016-10-07 10:56:52 +0530

    project report structured

--- page/projectandtaskreport.php
+++ page/projectandtaskreport.php
@@ -12,20 +12,25 @@ class page_projectandtaskreport extends page_reportsidebar{
 		********************************************************************/
 		$from_date = $this->app->stickyGET('from_date')?:$this->app->today;
 		$to_date = $this->app->stickyGET('to_date')?:$this->app->nextDate($this->app->today);
+		$project_id = $this->app->stickyGET('project_id');
+		$task_id = $this->app->stickyGET('task_id');
 		
 		/*******************************************************************
-		 EMPLOYEE MODEL AND EXPRESSIONS
+		 EMPLOYEE TIMESHEET AND EXPRESSIONS
 		********************************************************************/
 		$timesheet = $this->add('xepan\projects\Model_Timesheet');
-		
+		$timesheet->addCondition('task_id',$task_id);
+
 		/*******************************************************************
 	 	 FORM TO ENTER INFORMATION
 		********************************************************************/	
+		$task = $this->add('xepan\projects\Model_Task');
 		$form = $this->add('Form');
 		$form->addField('DatePicker','from_date')->set($this->app->today);
 		$form->addField('DatePicker','to_date')->set($this->app->today);
-		$form->addField('Dropdown','projects');
-		$form->addField('Dropdown','tasks');
+		$form->addField('Dropdown','projects')->setModel('xepan\projects\Project');
+		$task_field = $form->addField('autocomplete/Basic','task');
+		$task_field->setModel($task);
 		$form->addSubmit('Get Report')->addclass('btn btn-primary btn-sm btn-block');	
 
 		// GRID WILL BE ADDED ON THIS VIEW
@@ -44,6 +49,7 @@ class page_projectandtaskreport extends page_reportsidebar{
 			$array = [
 						'from_date'=>$form['from_date'],
 						'to_date'=>$form['to_date'],
+						'task_id'=>$form['task'],
 					 ];
 			$view->js()->reload($array)->execute();
 		}
