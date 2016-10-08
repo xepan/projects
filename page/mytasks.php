541f6fa89619021e5a2705930b5aedb6ac0cd24f
vaibhav <vaibhavsharma38@gmail.com>
2016-10-08 13:33:28 +0530

    mytask and instant task view

--- page/mytasks.php
+++ page/mytasks.php
@@ -7,13 +7,51 @@ class page_mytasks extends \xepan\base\Page{
 	function init(){
 		parent::init();
 
-		$task = $this->add('xepan\projects\Model_Task');
-		$task->addCondition('employee_id',$this->app->employee->id);
-		$task->setOrder('created_at','desc');
 
-		$task_view = $this->add('xepan\projects\View_TaskList',null,'my_task_view');
-		$task_view->setModel($task);
-		$task_view->add('xepan\hr\Controller_ACL',['action_btn_group'=>'xs']);
+		$task_assigned_to_me = $this->add('xepan\projects\View_TaskList',null,'leftview');	    
+	    $task_assigned_by_me = $this->add('xepan\projects\View_TaskList',null,'middleview');	    
+	    $task_waiting_for_approval = $this->add('xepan\projects\View_TaskList',null,'rightview');	    
+
+	    $task_assigned_to_me->template->trySet('task_view_title','Assigned To Me');
+	    $task_assigned_by_me->template->trySet('task_view_title','Assigned By Me');
+		$task_waiting_for_approval->template->trySet('task_view_title','Submitted To Me');
+
+		$task_assigned_to_me->add('xepan\base\Controller_Avatar',['name_field'=>'created_by','extra_classes'=>'profile-img center-block','options'=>['size'=>50,'display'=>'block','margin'=>'auto'],'float'=>null,'model'=>$this->model]);
+		$task_assigned_by_me->add('xepan\base\Controller_Avatar',['name_field'=>'created_by','extra_classes'=>'profile-img center-block','options'=>['size'=>50,'display'=>'block','margin'=>'auto'],'float'=>null,'model'=>$this->model]);
+		$task_waiting_for_approval->add('xepan\base\Controller_Avatar',['name_field'=>'created_by','extra_classes'=>'profile-img center-block','options'=>['size'=>50,'display'=>'block','margin'=>'auto'],'float'=>null,'model'=>$this->model]);
+
+		$status = 'Completed';
+
+	    $task_assigned_to_me_model = $this->add('xepan\projects\Model_Formatted_Task');
+	    $task_assigned_to_me_model
+	    			->addCondition(
+	    				$task_assigned_to_me_model->dsql()->orExpr()
+	    					->where('assign_to_id',$this->app->employee->id)
+	    					->where(
+    								$task_assigned_to_me_model->dsql()->andExpr()
+    									->where('created_by_id',$this->app->employee->id)
+    									->where('assign_to_id',null)
+	    							)
+	    				);
+
+	    $task_assigned_by_me_model = $this->add('xepan\projects\Model_Formatted_Task')
+										  ->addCondition('created_by_id',$this->app->employee->id)
+										  ->addCondition('assign_to_id','<>',$this->app->employee->id)
+										  ->addCondition('assign_to_id','<>',null)
+										  ->addCondition('status','<>','Submitted');
+
+	    $task_waiting_for_approval_model = $this->add('xepan\projects\Model_Formatted_Task')
+										  ->addCondition('created_by_id',$this->app->employee->id)
+										  ->addCondition('assign_to_id','<>',$this->app->employee->id)
+										  ->addCondition('assign_to_id','<>',null)
+										  ->addCondition('status','Submitted');	
+		
+		$task_assigned_to_me->setModel($task_assigned_to_me_model);
+		$task_assigned_by_me->setModel($task_assigned_by_me_model);
+		$task_waiting_for_approval->setModel($task_waiting_for_approval_model);
+
+
+
 	
 		$vp = $this->add('VirtualPage');
 		$vp->set(function($p){
@@ -23,9 +61,9 @@ class page_mytasks extends \xepan\base\Page{
 			$p->add('xepan\projects\View_Detail',['task_id'=>$task_id,'project_id'=>$project_id]);
 		});	
 
-		$task_view->js('click')->_selector('.task-item')->univ()->frameURL('TASK DETAIL',[$this->api->url($vp->getURL()),'task_id'=>$this->js()->_selectorThis()->closest('[data-id]')->data('id')]);
+		// $task_view->js('click')->_selector('.task-item')->univ()->frameURL('TASK DETAIL',[$this->api->url($vp->getURL()),'task_id'=>$this->js()->_selectorThis()->closest('[data-id]')->data('id')]);
 	
-		$task_view->js('click',$task_view->js()->reload(['delete_task_id'=>$this->js()->_selectorThis()->data('id')]))->_selector('.do-delete');
+		// $task_view->js('click',$task_view->js()->reload(['delete_task_id'=>$this->js()->_selectorThis()->data('id')]))->_selector('.do-delete');
 
 		if($_GET['delete_task_id']){
 			$delete_task=$this->add('xepan\projects\Model_Task');
