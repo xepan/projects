c0076b834a867c82272d41a90bdfd8c40fd264e6
sinharakesh <rksinha.btech@gmail.com>
2016-10-12 12:06:37 +0530

    pending project list addon at epan dashboard page

--- lib/Initiator.php
+++ lib/Initiator.php
@@ -31,9 +31,36 @@ class Initiator extends \Controller_Addon {
 
 		$search_project = $this->add('xepan\projects\Model_Project');
 		$this->app->addHook('quick_searched',[$search_project,'quickSearch']);
+		$this->app->addHook('epan_dashboard_page',[$this,'epanDashboard']);
 		return $this;
 
 	}
+
+	function epanDashboard($layout,$page){
+		
+		$task_assigned_to_me = $page->add('xepan\hr\CRUD',['allow_add'=>null,'grid_class'=>'xepan\projects\View_TaskList']);		
+		$task_assigned_to_me->add('xepan\base\Controller_Avatar',['name_field'=>'created_by','extra_classes'=>'profile-img center-block','options'=>['size'=>50,'display'=>'block','margin'=>'auto'],'float'=>null,'model'=>$this->model]);
+		$task_assigned_to_me->grid->template->trySet('task_view_title','My Pending Task');
+
+		if(!$task_assigned_to_me->isEditing())
+			$task_assigned_to_me->grid->addPaginator(25);
+
+		$task_assigned_to_me_model = $page->add('xepan\projects\Model_Formatted_Task');
+	    $task_assigned_to_me_model
+	    			->addCondition('status',['Pending','Inprogress'])
+	    			->addCondition(
+	    				$task_assigned_to_me_model->dsql()->orExpr()
+	    					->where('assign_to_id',$this->app->employee->id)
+	    					->where(
+    								$task_assigned_to_me_model->dsql()->andExpr()
+    									->where('created_by_id',$this->app->employee->id)
+    									->where('assign_to_id',null)
+	    							)
+	    				);
+
+	    $task_assigned_to_me->setModel($task_assigned_to_me_model)->setOrder('updated_at','desc');			
+	}
+
 	function setup_frontend(){
 		$this->routePages('xepan_projects');
 		$this->addLocation(array('template'=>'templates','js'=>'templates/js'))
