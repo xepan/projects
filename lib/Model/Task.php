e71f9d93279de4d7e9f36e0037876715910a63d3
vijay mali <guddavijay@gmail.com>
2016-10-17 12:09:08 +0530

    comment epan_id

--- lib/Model/Task.php
+++ lib/Model/Task.php
@@ -21,7 +21,7 @@ class Model_Task extends \xepan\base\Model_Table
 	{
 		parent::init();
 
-		$this->hasOne('xepan\base\Epan');
+		// $this->hasOne('xepan\base\Epan');
 		$this->hasOne('xepan\projects\Project','project_id');
 		$this->hasOne('xepan\hr\Employee','assign_to_id')->defaultValue($this->app->employee->id);
 		$this->hasOne('xepan\hr\Employee','created_by_id')->defaultValue($this->app->employee->id);
