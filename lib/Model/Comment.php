e37009f5929575b15318e50ffb24260ea1b0a863
dkanojia <dkanojia93@gmail.com>
2016-10-22 15:38:50 +0530

    in working..

--- lib/Model/Comment.php
+++ lib/Model/Comment.php
@@ -50,7 +50,7 @@ class Model_Comment extends \xepan\base\Model_Table
 			$task_name = $task['task_name'];
 
 		$this->app->employee->
-		addActivity("Comment On Task: '".$task_name."' Comment By'".$this->app->employee['name']."'",null, $this['employee_id'] /*Related Contact ID*/,null,null,null)->
+		addActivity(" '"$this['comment'] ."' commented on Task: '".$task_name."' by'".$this->app->employee['name']."'",null, $this['employee_id'] /*Related Contact ID*/,null,null,null)->
 		notifyTo([$this['employee_id']],"Comment on Task : " . $task_name);
 	}
 
---
+++ lib/Model/CommunicationMessages.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace xepan\projects;
+
+class Model_CommunicationMessages extends \xepan\base\Model_Table
+{	
+	function init()
+	{
+		parent::init();
+	}
+/**
+Project Application
+*/
+/**
+HR Application
+*/
+/**
+Commerce Application
+*/
+/**
+Marketing Application
+*/
+/**
+Account Application
+*/
+/**
+Production Application
+*/
+/**
+CRM Application
+*/
+/**
+CMS Application
+*/
+/**
+BLOG Application
+*/
+}
