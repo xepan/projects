fa2b37e94832409db34833ef272a80fb957bcb9a
vaibhav <vaibhavsharma38@gmail.com>
2016-11-04 12:57:03 +0530

    date range implemented in project dashboard

--- page/projectdashboard.php
+++ page/projectdashboard.php
@@ -7,56 +7,88 @@ class page_projectdashboard extends \xepan\projects\page_sidemenu{
 	function init(){
 		parent::init();
 
+		$this->start_date = $start_date = $_GET['start_date']?:date("Y-m-d", strtotime('-29 days', strtotime($this->app->today))); 		
+		$this->end_date = $end_date = $_GET['end_date']?:$this->app->today;
+				
+		// // HEADER FORM
+		$form = $this->add('Form',null,'form_layout');
+		$fld = $form->addField('DateRangePicker','period')
+                ->setStartDate($start_date)
+                ->setEndDate($end_date);
 
+        $this->end_date = $this->app->nextDate($this->end_date);
+		$form->addSubmit("Filter")->addClass('btn btn-primary');
+		
+		if($form->isSubmitted()){
+			$form->app->redirect($this->app->url(null,['start_date'=>$fld->getStartDate()?:0,'end_date'=>$fld->getEndDate()?:0]));
+		}
 
 		// Communications by staff 
      	$model = $this->add('xepan\hr\Model_Employee');
      	$model->addCondition('status','Active');
-		
+
 		$model->addExpression('pending_works')->set(function($m,$q){
-			return $this->add('xepan\projects\Model_Task')
-						->addCondition('assign_to_id',$q->getField('id'))
-						->addCondition('created_by_id','<>',$q->getField('id'))
-						->addCondition('status','Pending')
-						->count();
+			$task = $this->add('xepan\projects\Model_Task');
+			$task->addCondition('assign_to_id',$q->getField('id'))
+				 ->addCondition('created_by_id','<>',$q->getField('id'))
+				 ->addCondition('status','Pending')
+				 ->addCondition($task->dsql()->andExpr()
+				 ->where('created_at','>=',$this->start_date)
+				 ->where('created_at','<=',$this->app->nextDate($this->end_date)));
+			return $task->count();
 		});
 
 		$model->addExpression('please_receive')->set(function($m,$q){
-			return $this->add('xepan\projects\Model_Task')
-						->addCondition('assign_to_id',$q->getField('id'))
-						->addCondition('created_by_id','<>',$q->getField('id'))
-						->addCondition('status','Assigned')
-						->count();
+			$task = $this->add('xepan\projects\Model_Task');
+			$task->addCondition('assign_to_id',$q->getField('id'))
+      	         ->addCondition('created_by_id','<>',$q->getField('id'))
+				 ->addCondition('status','Assigned')
+				 ->addCondition($task->dsql()->andExpr()
+				 ->where('created_at','>=',$this->start_date)
+				 ->where('created_at','<=',$this->app->nextDate($this->end_date)));
+            return $task->count();
 		});
 
 		$model->addExpression('received_so_far')->set(function($m,$q){
-			return $this->add('xepan\projects\Model_Task')
-						->addCondition('assign_to_id',$q->getField('id'))
-						->addCondition('created_by_id','<>',$q->getField('id'))
-						->count();
+			$task = $this->add('xepan\projects\Model_Task');
+			$task->addCondition('assign_to_id',$q->getField('id'))
+				 ->addCondition('created_by_id','<>',$q->getField('id'))
+				 ->addCondition($task->dsql()->andExpr()
+				 ->where('created_at','>=',$this->start_date)
+				 ->where('created_at','<=',$this->app->nextDate($this->end_date)));	 
+			return $task->count();
 		});
 
 		$model->addExpression('total_tasks_assigned')->set(function($m,$q){
-			return $this->add('xepan\projects\Model_Task')
-						->addCondition('assign_to_id','<>',$q->getField('id'))
-						->addCondition('created_by_id',$q->getField('id'))
-						->count();
+			$task = $this->add('xepan\projects\Model_Task');
+			$task->addCondition('assign_to_id','<>',$q->getField('id'))
+                 ->addCondition('created_by_id',$q->getField('id'))
+                 ->addCondition($task->dsql()->andExpr()
+				 ->where('created_at','>=',$this->start_date)
+				 ->where('created_at','<=',$this->app->nextDate($this->end_date)));
+			return $task->count();
 		});
 
 		$model->addExpression('take_report_on_pending')->set(function($m,$q){
-			return $this->add('xepan\projects\Model_Task')
-						->addCondition('assign_to_id','<>',$q->getField('id'))
-						->addCondition('created_by_id',$q->getField('id'))
-						->addCondition('status',['Pending','Assigned'])
-						->count();
+			$task  = $this->add('xepan\projects\Model_Task');
+			$task->addCondition('assign_to_id','<>',$q->getField('id'))
+                 ->addCondition('created_by_id',$q->getField('id'))
+				 ->addCondition('status',['Pending','Assigned'])
+				 ->addCondition($task->dsql()->andExpr()
+				 ->where('created_at','>=',$this->start_date)
+				 ->where('created_at','<=',$this->app->nextDate($this->end_date)));
+			return $task->count();
 		});
 
 		$model->addExpression('check_submitted')->set(function($m,$q){
-			return $this->add('xepan\projects\Model_Task')
-						->addCondition('assign_to_id','<>',$q->getField('id'))
-						->addCondition('created_by_id',$q->getField('id'))
-						->addCondition('status','Submitted')
-						->count();
+			$task = $this->add('xepan\projects\Model_Task');
+			$task->addCondition('assign_to_id','<>',$q->getField('id'))
+				 ->addCondition('created_by_id',$q->getField('id'))
+				 ->addCondition('status','Submitted')
+				 ->addCondition($task->dsql()->andExpr()
+				 ->where('created_at','>=',$this->start_date)
+				 ->where('created_at','<=',$this->app->nextDate($this->end_date)));
+			return $task->count();
 		});
 
      	$this->add('xepan\base\View_Chart',null,'Charts')
@@ -75,13 +107,19 @@ class page_projectdashboard extends \xepan\projects\page_sidemenu{
 
 		$project->addExpression('Estimate')->set(function($m,$q){
 			$task = $this->add('xepan\projects\Model_Task');
-			$task->addCondition('project_id',$m->getElement('id'));
+			$task->addCondition('project_id',$m->getElement('id'))
+				 ->addCondition($task->dsql()->andExpr()
+				 ->where('created_at','>=',$this->start_date)
+				 ->where('created_at','<=',$this->app->nextDate($this->end_date)));	
 			return $task->_dsql()->del('fields')->field($q->expr('sum([0])',[$task->getElement('estimate_time')]));
 		});
 
 		$project->addExpression('Alloted')->set(function($m,$q){
 			$task = $this->add('xepan\projects\Model_Task');
-			$task->addCondition('project_id',$m->getElement('id'));
+			$task->addCondition('project_id',$m->getElement('id'))
+				 ->addCondition($task->dsql()->andExpr()
+				 ->where('created_at','>=',$this->start_date)
+				 ->where('created_at','<=',$this->app->nextDate($this->end_date)));	
 
 			$task->addExpression('diff_time')->set(function($m,$q){
 				return $q->expr('TIMESTAMPDIFF([0],[1],[2])',
@@ -93,7 +131,10 @@ class page_projectdashboard extends \xepan\projects\page_sidemenu{
 		$project->addExpression('Consumed')->set(function($m,$q){
 			$task = $this->add('xepan\projects\Model_Task');
 			$task->addCondition('project_id',$m->getElement('id'));
-			$task->addCondition('status','Completed');
+			$task->addCondition('status','Completed')
+				 ->addCondition($task->dsql()->andExpr()
+				 ->where('created_at','>=',$this->start_date)
+				 ->where('created_at','<=',$this->app->nextDate($this->end_date)));	
 
 			$task->addExpression('diff_time')->set(function($m,$q){
 				return $q->expr('TIMESTAMPDIFF([0],[1],[2])',
