e5be348c9f08a0f144da8e557c0c81f70ee4984a
vaibhavsharma38 <vaibhavsharma38@gmail.com>
2017-01-28 17:48:41 +0530

    worked on task restructuring

--- page/test.php
+++ page/test.php
@@ -5,8 +5,35 @@ class page_test extends \Page{
 	function init(){
 		parent::init();
 
+		$form = $this->add('Form');
+		$form->addField('DropDown','do_what')->setValueList(['CallCron'=>'CallCron','CompatibleToRestructure'=>'CompatibleToRestructure'])->setEmptyText('Please select a value');
+		$form->addSubmit('Submit');
+		
+		if($form->isSubmitted()){
+			if($form['do_what'] == 'CallCron')
+				$this->CallCron();
+							
+			if($form['do_what'] == 'CompatibleToRestructure')				
+				$this->CompatibleToRestructure();
+		}	
+	}
+
+	function CallCron(){		
 		$task = $this->add('xepan\projects\Model_Task');
 		$task->reminder();
 		$task->recurring();
+	}
+
+	function compatibleToRestructure(){
+		$tasks = $this->add('xepan\projects\Model_Task');
+		$tasks->addCondition('set_reminder',true);
+		$tasks->addCondition([['is_reminded',false],['is_reminded',null]]);
+
+		foreach ($tasks as $task) {
+			$reminder_time = date("Y-m-d H:i:s", strtotime('- '.$task['remind_value'].' '.$task['remind_unit'], strtotime($task['starting_date'])));
+			$task['reminder_time'] = $reminder_time;
+			$task['snooze_duration'] = null;
+		}
+		
 	}
 }
