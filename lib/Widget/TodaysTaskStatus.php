b30f289dfee5bd34c147929c5cd83011816d4063
vaibhavsharma38 <vaibhavsharma38@gmail.com>
2017-02-25 10:20:45 +0530

    todays task status widget made

---
+++ lib/Widget/TodaysTaskStatus.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace xepan\projects;
+
+class Widget_TodaysTaskStatus extends \xepan\base\Widget{
+	function init(){
+		parent::init();
+
+		$this->report->enableFilterEntity('employee');
+		$this->view = $this->add('View',null,null,['widget\todays-task-status']);
+	}
+
+	function recursiveRender(){
+		$employee_id = isset($this->report->employee)?$this->report->employee : $this->app->employee->id;
+		
+		$employee = $this->add('xepan\hr\Model_Employee');
+		$employee->load($employee_id);
+
+		$this->view->template->trySet('employee',$employee['name']);
+		$this->view->template->trySet('date',$this->app->today);
+
+		$task_created_m = $this->add('xepan\projects\Model_Task');
+		$task_created_m->addCondition('type','Task');
+		$task_created_m->addCondition('created_by_id',$employee_id);
+		$task_created_m->addCondition('created_at','>=',$this->app->today);
+		$task_created_count = $task_created_m->count();
+		$this->view->template->trySet('created',$task_created_count);
+
+		$task_pending_m = $this->add('xepan\projects\Model_Task');
+		$task_pending_m->addCondition('type','Task');
+		$task_pending_m->addCondition('assign_to_id',$employee_id);
+		$task_pending_m->addCondition('status','Pending');
+		$task_pending_count = $task_pending_m->count();
+		$this->view->template->trySet('pending',$task_pending_count);
+
+		$task_toreceive_m = $this->add('xepan\projects\Model_Task');
+		$task_toreceive_m->addCondition('type','Task');
+		$task_toreceive_m->addCondition('assign_to_id',$employee_id);
+		$task_toreceive_m->addCondition('status','Assigned');
+		$task_toreceive_count = $task_toreceive_m->count();
+		$this->view->template->trySet('to_receive',$task_toreceive_count);
+
+		$task_submitted_m = $this->add('xepan\projects\Model_Task');
+		$task_submitted_m->addCondition('type','Task');
+		$task_submitted_m->addCondition('assign_to_id',$employee_id);
+		$task_submitted_m->addCondition('status','Submitted');
+		$task_submitted_count = $task_submitted_m->count();
+		$this->view->template->trySet('submitted',$task_submitted_count);
+
+		$task_completed_m = $this->add('xepan\projects\Model_Task');
+		$task_completed_m->addCondition('type','Task');
+		$task_completed_m->addCondition('assign_to_id',$employee_id);
+		$task_completed_m->addCondition('status','Completed');
+		$task_completed_m->addCondition('created_at','>=',$this->app->now);
+		$task_completed_count = $task_completed_m->count();
+		$this->view->template->trySet('completed',$task_completed_count);
+
+		parent::recursiveRender();
+	}
+}
