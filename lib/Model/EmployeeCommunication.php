928e805024be08a5d327d2b279d58a76e17f75a3
vijay mali <guddavijay@gmail.com>
2017-08-05 15:31:05 +0530

    employee communication report done

--- lib/Model/EmployeeCommunication.php
+++ lib/Model/EmployeeCommunication.php
@@ -7,14 +7,16 @@ class Model_EmployeeCommunication extends \xepan\hr\Model_Employee{
 	public $to_date;
 	function init(){
 		parent::init();
+		// echo "string".$this->from_date;
+		
 		$this->addExpression('assign_to_pending_task')->set(function($m,$q){
 			// return $q->getField('id');
-			return $this->add('xepan\projects\Model_Task',['table_alias'=>'employee_assign_to_assigntask'])
-						->addCondition('assign_to_id',$q->getField('id'))
+			$task = $this->add('xepan\projects\Model_Task',['table_alias'=>'employee_assign_to_assigntask']);
+				return 	$task->addCondition('assign_to_id',$q->getField('id'))
 						->addCondition('assign_to_id','<>',null)
 						->addCondition('status','Pending')
-						// ->addCondition('created_at','>=',$this->from_date)
-						// ->addCondition('created_at','<=',$this->to_date)
+						->addCondition('created_at','>=',$this->from_date)
+						->addCondition('created_at','<',$this->api->nextDate($this->to_date))
 						->count();
 		});
 
@@ -23,8 +25,8 @@ class Model_EmployeeCommunication extends \xepan\hr\Model_Employee{
 						->addCondition('assign_to_id',$q->getField('id'))
 						->addCondition('assign_to_id','<>',null)
 						->addCondition('status','Inprogress')
-						// ->addCondition('created_at','>=',$this->from_date)
-						// ->addCondition('created_at','<=',$this->to_date)
+						->addCondition('created_at','>=',$this->from_date)
+						->addCondition('created_at','<',$this->api->nextDate($this->to_date))
 						->count();
 		});
 		$this->addExpression('assign_to_complete_task')->set(function($m,$q){
@@ -32,8 +34,8 @@ class Model_EmployeeCommunication extends \xepan\hr\Model_Employee{
 						->addCondition('assign_to_id',$q->getField('id'))
 						->addCondition('assign_to_id','<>',null)
 						->addCondition('status','Completed')
-						// ->addCondition('created_at','>=',$this->from_date)
-						// ->addCondition('created_at','<=',$this->to_date)
+						->addCondition('created_at','>=',$this->from_date)
+						->addCondition('created_at','<',$this->api->nextDate($this->to_date))
 						->count();
 		});
 
@@ -43,8 +45,8 @@ class Model_EmployeeCommunication extends \xepan\hr\Model_Employee{
 						->addCondition('created_by_id',$q->getField('id'))
 						->addCondition('created_by_id','<>',null)
 						->addCondition('status','Pending')
-						// ->addCondition('created_at','>=',$this->from_date)
-						// ->addCondition('created_at','<=',$this->to_date)
+						->addCondition('created_at','>=',$this->from_date)
+						->addCondition('created_at','<',$this->api->nextDate($this->to_date))
 						->count();
 		});
 
@@ -53,8 +55,8 @@ class Model_EmployeeCommunication extends \xepan\hr\Model_Employee{
 						->addCondition('created_by_id',$q->getField('id'))
 						->addCondition('created_by_id','<>',null)
 						->addCondition('status','Inprogress')
-						// ->addCondition('created_at','>=',$this->from_date)
-						// ->addCondition('created_at','<=',$this->to_date)
+						->addCondition('created_at','>=',$this->from_date)
+						->addCondition('created_at','<',$this->api->nextDate($this->to_date))
 						->count();
 		});
 		$this->addExpression('assign_by_complete_task')->set(function($m,$q){
@@ -62,8 +64,8 @@ class Model_EmployeeCommunication extends \xepan\hr\Model_Employee{
 						->addCondition('created_by_id',$q->getField('id'))
 						->addCondition('created_by_id','<>',null)
 						->addCondition('status','Completed')
-						// ->addCondition('created_at','>=',$this->from_date)
-						// ->addCondition('created_at','<=',$this->to_date)
+						->addCondition('created_at','>=',$this->from_date)
+						->addCondition('created_at','<',$this->api->nextDate($this->to_date))
 						->count();
 		});
 
@@ -77,80 +79,86 @@ class Model_EmployeeCommunication extends \xepan\hr\Model_Employee{
 					->where('assign_to_id',null)));
 			$task->addCondition('deadline','<',$this->app->now);			
 			$task->addCondition('status','<>','Completed');
+			$task->addCondition('created_at','>=',$this->from_date);
+			$task->addCondition('created_at','<',$this->api->nextDate($this->to_date));
 			return $task->count();		
 		});
 
-		$this->addExpression('total_received_message')->set(function($m,$q){
-			return $this->add('xepan\communication\Model_Communication_MessageReceived')
-						->addCondition('to_id',$q->getField('id'))
-						->count();
+		// $this->addExpression('total_received_message')->set(function($m,$q){
+		// 	return $this->add('xepan\communication\Model_Communication_MessageReceived')
+		// 				->addCondition('to_id',$q->getField('id'))
+		// 				->count();
 
-		});
+		// });
 		$this->addExpression('total_send_message')->set(function($m,$q){
 			return $this->add('xepan\communication\Model_Communication_MessageSent')
+						->addCondition('created_at','>=',$this->from_date)
+						->addCondition('created_at','<',$this->api->nextDate($this->to_date))
 						->addCondition('created_by_id',$q->getField('id'))
 						->count();
 		});
-		$this->addExpression('total_received_emails')->set(function($m,$q){
-			return $this->add('xepan\communication\Model_Communication_Email_Received')
-						->addCondition('to_id',$q->getField('id'))
-						->count();
+		// $this->addExpression('total_received_emails')->set(function($m,$q){
+		// 	return $this->add('xepan\communication\Model_Communication_Email_Received')
+		// 				->addCondition('to_id',$q->getField('id'))
+		// 				->count();
 
-		});
+		// });
 		$this->addExpression('total_send_emails')->set(function($m,$q){
 			return $this->add('xepan\communication\Model_Communication_Email_Sent')
 						->addCondition('created_by_id',$q->getField('id'))
+						->addCondition('created_at','>=',$this->from_date)
+						->addCondition('created_at','<',$this->api->nextDate($this->to_date))
 						->count();
 		});
 		// $this->addExpression('total_received_emails');
 		// $this->addExpression('total_send_emails');
 		
-		$this->addExpression('running_task')->set(function($m,$q){
-			return $this->add('xepan\projects\Model_Timesheet',['table_alias'=>'running_task'])
-						->addCondition('endtime',null)
-						->addCondition('employee_id',$q->getField('id'))
-						->setOrder('starttime','desc')
-						->setLimit(1)
-						->fieldQuery('task');
-		})->sortable(true);
+		// $this->addExpression('running_task')->set(function($m,$q){
+		// 	return $this->add('xepan\projects\Model_Timesheet',['table_alias'=>'running_task'])
+		// 				->addCondition('endtime',null)
+		// 				->addCondition('employee_id',$q->getField('id'))
+		// 				->setOrder('starttime','desc')
+		// 				->setLimit(1)
+		// 				->fieldQuery('task');
+		// })->sortable(true);
 
 
-		$this->addExpression('running_task_id')->set(function($m,$q){
-			return $this->add('xepan\projects\Model_Timesheet',['table_alias'=>'running_task'])
-						->addCondition('endtime',null)
-						->addCondition('employee_id',$q->getField('id'))
-						->setOrder('starttime','desc')
-						->setLimit(1)
-						->fieldQuery('task_id');
-		})->sortable(true);
+		// $this->addExpression('running_task_id')->set(function($m,$q){
+		// 	return $this->add('xepan\projects\Model_Timesheet',['table_alias'=>'running_task'])
+		// 				->addCondition('endtime',null)
+		// 				->addCondition('employee_id',$q->getField('id'))
+		// 				->setOrder('starttime','desc')
+		// 				->setLimit(1)
+		// 				->fieldQuery('task_id');
+		// })->sortable(true);
 
-		$this->addExpression('running_task_since')->set(function($m,$q){
-			return $this->add('xepan\projects\Model_Timesheet',['table_alias'=>'running_task'])
-						->addCondition('endtime',null)
-						->addCondition('employee_id',$q->getField('id'))
-						->setOrder('starttime','desc')
-						->setLimit(1)
-						->fieldQuery('duration');
-		})->sortable(true);
+		// $this->addExpression('running_task_since')->set(function($m,$q){
+		// 	return $this->add('xepan\projects\Model_Timesheet',['table_alias'=>'running_task'])
+		// 				->addCondition('endtime',null)
+		// 				->addCondition('employee_id',$q->getField('id'))
+		// 				->setOrder('starttime','desc')
+		// 				->setLimit(1)
+		// 				->fieldQuery('duration');
+		// })->sortable(true);
 
-		$this->addExpression('project')->set(function($m,$q){
-			$p=$this->add('xepan\projects\Model_Project');
-			$task_j = $p->join('task.project_id');
-			$task_j->addField('task_id','id');
-			$p->addCondition($q->expr('[0]=[1]',[$p->getElement('task_id'),$m->getField('running_task_id')]));
-			return $p->fieldQuery('name');
-		})->sortable(true);
+		// $this->addExpression('project')->set(function($m,$q){
+		// 	$p=$this->add('xepan\projects\Model_Project');
+		// 	$task_j = $p->join('task.project_id');
+		// 	$task_j->addField('task_id','id');
+		// 	$p->addCondition($q->expr('[0]=[1]',[$p->getElement('task_id'),$m->getField('running_task_id')]));
+		// 	return $p->fieldQuery('name');
+		// })->sortable(true);
 
-		$this->hasMany('xepan\projects\Task','assign_to_id');
+		// $this->hasMany('xepan\projects\Task','assign_to_id');
 
-		$this->addExpression('pending_tasks_count')->set(function ($m,$q){
-			return $m->refSQL('xepan\projects\Task')
-						->addCondition('status','Pending')
-						->count();
-		})->sortable(true);
+		// $this->addExpression('pending_tasks_count')->set(function ($m,$q){
+		// 	return $m->refSQL('xepan\projects\Task')
+		// 				->addCondition('status','Pending')
+		// 				->count();
+		// })->sortable(true);
 
 
 
-		$this->addExpression('performance')->set("'Todo'");
+		// $this->addExpression('performance')->set("'Todo'");
 	}
 }
--- page/report/employee.php
+++ page/report/employee.php
@@ -10,18 +10,24 @@ class page_report_employee extends \xepan\base\Page{
 	public $title = "Employee Communication Report";
 	function init(){
 		parent::init();
-
+		$from_date = $this->app->stickyGET('from_date');
+		$to_date = $this->app->stickyGET('to_date');
+		$set_date = $from_date." to ".$to_date;
 		$form = $this->add('Form',null,null,['form/empty']);
-		$date = $form->addField('DateRangePicker','date_range');	
+		$date = $form->addField('DateRangePicker','date_range')->set($set_date);	
 		$emp_field = $form->addField('xepan\base\Basic','employee');
 		$emp_field->setModel('xepan\projects\Model_EmployeeCommunication');
 		$dept_field = $form->addField('DropDown','department')->setEmptyText('Please Select Department');
 		$dept_field->setModel('xepan\hr\Model_Department');
 		$form->addSubmit('Get Details');
-			
+		// if($from_date)
+			// throw new \Exception($from_date, 1);
+					
 
-		$employee_comm = $this->add('xepan\projects\Model_EmployeeCommunication',['from_date'=>$_GET['from_date'],'to_date'=>$_GET['to_date']]);
-		
+		$employee_comm = $this->add('xepan\projects\Model_EmployeeCommunication',['from_date'=>$from_date,'to_date'=>$to_date]);
+		if($_GET['from_date']){
+			$employee_comm->from_date = $_GET['from_date'];
+		}		
 		if($_GET['employee_id']){
 			$employee_comm->addCondition('id',$_GET['employee_id']);
 		}
@@ -34,13 +40,13 @@ class page_report_employee extends \xepan\base\Page{
 
 		if($form->isSubmitted()){
 			
-			$grid->js()->reload(
-							[
+			$form->js()->univ()->redirect($this->app->url(),[
 								'employee_id'=>$form['employee'],
 								'department_id'=>$form['department'],
 								'from_date'=>$date->getStartDate()?:0,
 								'to_date'=>$date->getEndDate()?:0
 							]
+							
 						)->execute();
 		}
 	}
