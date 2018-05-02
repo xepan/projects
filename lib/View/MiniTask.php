ed473cafc46edcc15b3320fc543652c675ffdbba
Gowrav Vishwakarma <gowravvishwakarma@gmail.com>
2018-05-02 15:50:23 +0530

    open frameURL instead dialogURL

--- lib/View/MiniTask.php
+++ lib/View/MiniTask.php
@@ -52,7 +52,7 @@ class View_MiniTask extends \View{
 		if(!$task_list->running_task_id && $force_to_fill_sitting_ideal){
 
 			$task_list->js(true,"sitting_ideal_interval = setInterval(function(){\$.univ().notify('Sitting Ideal ?','You looks sitting idea, please tell what you are doing !','success',true,null,true,'warning');},".($force_to_fill_sitting_ideal?($force_to_fill_sitting_ideal*1000):60000).")");
-			$task_list->js(true)->_selector('.xepan-mini-task')->univ()->dialogURL('What are you doing now?',[$this->app->url('xepan_projects_instanttaskfeed')]);
+			$task_list->js(true)->_selector('.xepan-mini-task')->univ()->frameURL('What are you doing now?',[$this->app->url('xepan_projects_instanttaskfeed')]);
 		}else{
 			$task_list->js(true,'if(typeof sitting_ideal_interval != "undefined") clearInterval(sitting_ideal_interval)');
 		}
