a85991f8675110cb72bd07f41e3f97b1c87f0cd5
Gowrav Vishwakarma <gowravvishwakarma@gmail.com>
2018-05-29 10:28:59 +0530

    show existing followups on date when setting new communication & followup

---
+++ lib/View/EmployeeFollowupSchedule.php
@@ -0,0 +1,39 @@
+<?php
+
+
+namespace xepan\projects;
+
+class View_EmployeeFollowupSchedule extends \View {
+	public $employee_field;
+	public $date_field;
+	
+	function init(){
+		parent::init();
+
+		$emp_id = $this->app->stickyGET('employee_schedule');
+		$date = $this->app->stickyGET('date_schedule');
+		if(!$date || $date=='') $date = $this->app->today;
+
+		$this->employee_field->js('change',$this->js()->reload(['employee_schedule'=>$this->employee_field->js()->val(),'date_schedule'=>$this->date_field->js()->val()]));
+		$this->date_field->js('change',$this->js()->reload(['employee_schedule'=>$this->employee_field->js()->val(),'date_schedule'=>$this->date_field->js()->val()]));
+		
+		if(!$emp_id || !$date){
+			$this->add('View')->set('Please select employee and date to update');
+			return;
+		}
+
+		$employee = $this->add('xepan\hr\Model_Employee')->load($emp_id);
+
+		$model_task = $this->add('xepan\projects\Model_Task');
+		$model_task->addExpression('on_date')->set('DATE(starting_date)');
+		$model_task['type'] = 'Followup';
+		$model_task->addCondition('assign_to_id',$emp_id);
+		$model_task->addCondition('on_date',date('Y-m-d',strtotime($date)));
+
+		$g=$this->add('Grid');
+		$g->add('View',null,'grid_buttons')->set('Existing Schedules');
+		$g->setModel($model_task,['starting_date','task_name','assign_to']);
+
+	}
+
+}
