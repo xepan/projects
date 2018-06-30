92389a55a662528a99b40ec08a5f1e70c9854731
Gowrav Vishwakarma <gowravvishwakarma@gmail.com>
2018-06-30 17:06:57 +0530

    page updated as per updtaed view_calendar options

--- page/taskscalendar.php
+++ page/taskscalendar.php
@@ -8,16 +8,13 @@ class page_taskscalendar extends \xepan\base\Page {
 
 		$employee_field = $this->app->stickyGET('employee_field');
 		$date_field = $this->app->stickyGET('date_field');
-		$employee_only = $this->app->stickyGET('employee_only');
 		$type_of_tasks = $this->app->stickyGET('type_of_tasks');
 		
 		$m = $this->add('xepan\projects\Model_Task');
 		$m->setOrder('starting_date','desc');
-		if($employee_only)
-			$m->addCondition('assign_to_id',$employee_only);
 		if($type_of_tasks)
 			$m->addCondition('type',$type_of_tasks);
-		$v = $this->add('xepan\projects\View_TaskCalendar',['employee_field_to_set'=>"#".$employee_field,'startingdate_field_to_set'=>"#".$date_field,'employee_only'=>$employee_only]);
+		$v = $this->add('xepan\projects\View_TaskCalendar',['employee_field_to_set'=>"#".$employee_field,'startingdate_field_to_set'=>"#".$date_field,'defaultView'=>'month','title_field'=>'assign_to','add_employee_filter'=>true,'default_task_type'=>'Followup']);
 		$v->setModel($m);
 	}
 }
