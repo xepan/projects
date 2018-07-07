ca481b1bb3651b1d65a7d44c82ea1da2548f33a8
Gowrav Vishwakarma <gowravvishwakarma@gmail.com>
2018-07-07 15:03:26 +0530

    show task details on click

--- lib/View/TaskCalendar.php
+++ lib/View/TaskCalendar.php
@@ -35,6 +35,9 @@ class View_TaskCalendar extends \View{
 		$m = $this->add('xepan\projects\Model_Config_TaskSubtype')->tryLoadAny();
 		$this->task_sub_types = explode(",", $m['value']);
 
+		$this->vp = $this->add('VirtualPage');
+		$this->vp->set([$this,'openTask']);
+
 	}
 
 	function setModel($model){
@@ -44,7 +47,7 @@ class View_TaskCalendar extends \View{
 		$this->event_array=[];
 		// {title:value.title,start:value.start,document_id:value.document_id,'client_event_id':value._id}
 		foreach ($model as $m) {
-			$color = ($m['type']=='Followup' && $m['Status'] !='Completed' && strtotime($m['starting_date']) < strtotime($this->app->today)) ? 'red':null;
+			$color = ($m['type']=='Followup' && $m['status'] !='Completed' && strtotime($m['starting_date']) < strtotime($this->app->today)) ? 'red':null;
 			$e =['title'=>$m[$this->title_field],'start'=>$m['starting_date'],'task_id'=>$m->id,'allDay'=>false,'end'=>$m['starting_date'],'desc'=>$m->description(),'assign_to_id'=>$m['assign_to_id'],'type'=>$m['type'],'sub_type'=>$m['sub_type'],'color'=>$color];
 			$this->event_array[] = $e;
 		}
@@ -60,8 +63,18 @@ class View_TaskCalendar extends \View{
 		$this->js(true)->_css('fullcalendar-3.9.0/fullcalendar');//->_css('compiled/calendar');
 		$this->js(true)->_load('fullcalendar-3.9.0/lib/moment.min')->_load('fullcalendar-3.9.0/fullcalendar.min')->_load('xepan-followup-scheduler13');
 		// showFollowupCalendar: function(obj,events_passed,defaultView, employee_list, add_employee_filter, add_task_types_filter,default_task_type, add_task_sub_types_filter, task_sub_types, employee_field_to_set, startingdate_field_to_set,form){
-		$this->js(true)->univ()->showFollowupCalendar($this->calview,$this->event_array, $this->defaultView, $this->employee_list, $this->add_employee_filter, $this->add_task_types_filter, $this->default_task_type, $this->add_task_sub_types_filter, $this->task_sub_types, $this->employee_field_to_set, $this->startingdate_field_to_set,$this->form);
+		$this->js(true)->univ()->showFollowupCalendar($this->calview,$this->event_array, $this->defaultView, $this->employee_list, $this->add_employee_filter, $this->add_task_types_filter, $this->default_task_type, $this->add_task_sub_types_filter, $this->task_sub_types, $this->employee_field_to_set, $this->startingdate_field_to_set,$this->form,$this->vp->getURL());
 		return parent::render();
+	}
+
+
+	function openTask($p){
+		$task_id = $p->app->stickyGET('task_id');
+		// $task_view = $p->add('xepan\projects\View_Detail',['task_id'=>$task_id,'task_type'=>'followup']);	
+		$my_followups = $p->add('xepan\hr\CRUD',['entity_name'=>'Followup','grid_class'=>'xepan\projects\View_TaskList']);
+		$my_followups_model = $p->add('xepan\projects\Model_FollowUp');
+		$my_followups_model->addCondition('id',$task_id);
+		$my_followups->setModel($my_followups_model);
 	}
 
 }
